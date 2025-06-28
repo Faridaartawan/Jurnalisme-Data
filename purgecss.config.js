14f9f6aed2e0aeda19de38f766d5715b6d5eb9c9
Faridaartawan <farida.artawan@gmail.com>
2025-06-29 05:38:27 +0700

    asdasd

---
+++ purgecss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './index.html',
+      './kebijakan-privasi.html',
+      './assets/js/**/*.js',
+    ],
+    css: [
+      './assets/css/main.css',
+      './assets/css/style.min.css',
+    ],
+    output: './assets/cleaned/',
+  };
